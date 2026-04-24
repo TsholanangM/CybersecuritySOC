1ede7066f1d52db3c86139e7fd31ce9df951f47a
TsholanangM <tsholanangmashishi24@gmail.com>
2026-04-24 21:11:11 +0200

    Create CybersocApplication.java main application class

---
+++ backend/src/main/java/com/cybersoc/CybersocApplication.java
@@ -0,0 +1,12 @@
+package com.cybersoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CybersocApplication.class, args);
+    }
+}
